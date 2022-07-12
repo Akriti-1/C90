e43cf330a91c451df493000f4cff7f9f27721cbc
Akriti-1 <102719420+Akriti-1@users.noreply.github.com>
2022-07-12 17:42:52 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,21 @@
+
+	player1_name = localStorage.getItem("player_1");
+	player2_name = localStorage.getItem("player_2");
+
+	player1_score = 0;
+	player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+
+document.getElementById("player1_score").innerHTML = player1_score ;
+document.getElementById("player2_score").innerHTML = player2_score ;
+
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
+document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
+
+function Delete(){
+	localStorage.setItem("player_1", "");
+	localStorage.setItem("player_2", "");
+	window.location.replace("index.html");
+}
